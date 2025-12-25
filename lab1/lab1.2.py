744cb00e23c42ce169122288f6df2ea6b104679c
Frut1n <tvinkforbs1537@gmail.com>
2025-12-26 01:46:13 +0300

    lab1.2.py

--- lab1/lab1.2.py
+++ lab1/lab1.2.py
@@ -1,6 +1,7 @@
-a=int(input("ввведите a: "))
-b=int(input("ввведите b: "))
+a=int(input("Ввведите a: "))
+b=int(input("Ввведите b: "))
 if a==b:
     print('Одинаковые')
 else:
+
     print("Наибольшее число:", max(a, b))
