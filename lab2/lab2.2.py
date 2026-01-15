fa772677df354aba12e1873101a6d0d8fbbcd5d2
Frut1n <tvinkforbs1537@gmail.com>
2026-01-15 06:24:13 +0300

    исправил #2.2

--- lab2/lab2.2.py
+++ lab2/lab2.2.py
@@ -1,8 +1,9 @@
 def describe_person(name, age=30):
-    return ('Имя человека:', name, ', Возраст:', age)
+    print(f"Имя: {name}, Возраст: {age}")
 name1 = str(input('Введите имя: '))
 age = input('Введите возраст (или пропустите для авто-заполнения): ')
 if age == '':
     print (describe_person(name1))
 else:
+
     print(describe_person(name1, int(age)))
